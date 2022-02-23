452e860f6686289d5f0bcca98ab630447dc62206
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-02-23 20:48:15 +0100

    added footer

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faAngleDoubleUp, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  public faAngleDoubleUp: IconDefinition = faAngleDoubleUp;
 
   constructor() { }
 
