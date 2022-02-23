d844563567635fee19465db728eb0ab1264013ee
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-02-23 11:20:52 +0100

    added navbar

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faEnvelope, IconDefinition } from '@fortawesome/free-regular-svg-icons';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  public faEnvelope: IconDefinition = faEnvelope;
 
   constructor() { }
 
